99f0bcd35553fe1f843bfb975e0b6dd447327bbc
cipri321 <developer@exigea.ro>
2022-10-26 09:47:10 +0300

    resolved comments

---
+++ lab2/HashTable.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ciprian Marin on 19.10.2022.
+//
+
+#include "HashTable.h"
---
+++ lab2/HashTable.h
@@ -0,0 +1,16 @@
+//
+// Created by Ciprian Marin on 19.10.2022.
+//
+
+#ifndef LAB2_HASHTABLE_H
+#define LAB2_HASHTABLE_H
+
+
+
+class HashTable {
+
+};
+
+
+
+#endif //LAB2_HASHTABLE_H
